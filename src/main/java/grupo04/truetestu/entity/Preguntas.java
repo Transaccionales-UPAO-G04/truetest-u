6d00e0f3b476e0c4ab01aa46b1d6cb545e0de2eb
AngelOncoy <mau-1-21@hotmail.com>
2024-09-14 20:40:24 -0500

    feature/mapeo-preguntas-carrera-respuestas

--- src/main/java/grupo04/truetestu/entity/Preguntas.java
+++ src/main/java/grupo04/truetestu/entity/Preguntas.java
@@ -1,4 +1,29 @@
 package grupo04.truetestu.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name= "Preguntas")
+
 public class Preguntas {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private String pregunta;
+
+        @Column(name = "punto",nullable = false, length = 50)
+        private int punto;
+
+        @ManyToOne
+        @JoinColumn(name = "PruebaVocational_id", referencedColumnName = "idPruebaVocacional"
+        , foreignKey = @ForeignKey(name = "FK_purchase_customer"))
+        private PruebaVocacional pruebaVocacional;
+
+        @OneToMany(mappedBy = "preguntas", cascade = CascadeType.ALL)
+        private List<Respuestas> resp;
+
 }
