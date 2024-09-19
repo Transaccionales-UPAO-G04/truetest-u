0ceb7cef6e55d1e6b562e68609dc1276bb885ca1
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-09-19 17:52:57 -0500

    Visualizar Especialidad

--- src/main/java/grupo04/truetestu/dto/ResultadoPruebaDTO.java
+++ src/main/java/grupo04/truetestu/dto/ResultadoPruebaDTO.java
@@ -6,5 +6,4 @@ import lombok.Data;
 public class ResultadoPruebaDTO {
     private int puntaje;
     private String recomendacion;
-    private int idPruebaVocacional; // Si necesitas referenciar a la prueba vocacional.
 }
