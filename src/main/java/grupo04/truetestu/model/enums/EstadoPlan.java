4653e917d4bfe7bc4bb23e4b383c4f17e58ef2f1
Briyan Anthony Dávila Ortiz <bdavilao2@upao.edu.pe>
2024-09-18 23:42:14 -0500

    inhabilitar estudiante implementado

--- src/main/java/grupo04/truetestu/model/enums/EstadoPlan.java
+++ src/main/java/grupo04/truetestu/model/enums/EstadoPlan.java
@@ -1,6 +1,6 @@
 package grupo04.truetestu.model.enums;
 
-public enum EstadoEstudiante {
+public enum EstadoPlan {
     PREMIUM,
     NOPREMIUM
 }
--- src/main/java/grupo04/truetestu/service/EstudianteService.java
+++ src/main/java/grupo04/truetestu/service/EstudianteService.java
@@ -1,9 +1,14 @@
 package grupo04.truetestu.service;
 
 import grupo04.truetestu.model.entity.Estudiante;
+import grupo04.truetestu.model.enums.EstadoPlan;
 
 public interface EstudianteService {
 
     Estudiante registerEstudiante(Estudiante estudiante);
+
+    Estudiante findById(int id);
+    void inhabilitarCuenta(int id);
+    void cambiarPlan(int id, EstadoPlan nuevoEstadoPlan);
 
 }
