43e906aa4ec7a78d41581b4e936a36851595bc73
Briyan Anthony Dávila Ortiz <bdavilao2@upao.edu.pe>
2024-09-18 23:42:39 -0500

    CRUD Mentor Implementado

---
+++ src/main/java/grupo04/truetestu/api/MentorController.java
@@ -0,0 +1,51 @@
+package grupo04.truetestu.api;
+
+import grupo04.truetestu.model.entity.Mentor;
+import grupo04.truetestu.service.MentorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mentores")
+public class MentorController {
+
+    @Autowired
+    private MentorService mentorService;
+
+    // Obtener todos los mentores
+    @GetMapping
+    public List<Mentor> findAll() {
+        return mentorService.findAll();
+    }
+
+    // Obtener un mentor por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Mentor> findById(@PathVariable int id) {
+        Mentor mentor = mentorService.findById(id);
+        return ResponseEntity.ok(mentor);
+    }
+
+    // Crear un nuevo mentor
+    @PostMapping
+    public ResponseEntity<Mentor> createMentor(@RequestBody Mentor mentor) {
+        Mentor nuevoMentor = mentorService.createMentor(mentor);
+        return ResponseEntity.ok(nuevoMentor);
+    }
+
+    // Actualizar un mentor existente
+    @PutMapping("/{id}")
+    public ResponseEntity<Mentor> updateMentor(@PathVariable int id, @RequestBody Mentor mentorDetails) {
+        Mentor mentorActualizado = mentorService.updateMentor(id, mentorDetails);
+        return ResponseEntity.ok(mentorActualizado);
+    }
+
+    // Eliminar un mentor
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMentor(@PathVariable int id) {
+        mentorService.deleteMentor(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/grupo04/truetestu/service/impl/MentorServiceImpl.java
@@ -0,0 +1,53 @@
+package grupo04.truetestu.service.impl;
+
+import grupo04.truetestu.model.entity.Mentor;
+import grupo04.truetestu.repository.MentorRepository;
+import grupo04.truetestu.infra.exception.ResourceNotFoundException;
+import grupo04.truetestu.service.MentorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MentorServiceImpl implements MentorService {
+
+    @Autowired
+    private MentorRepository mentorRepository;
+
+    @Override
+    public List<Mentor> findAll() {
+        return mentorRepository.findAll();
+    }
+
+    @Override
+    public Mentor findById(int id) {
+        return mentorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Mentor no encontrado"));
+    }
+
+    @Override
+    @Transactional
+    public Mentor createMentor(Mentor mentor) {
+        return mentorRepository.save(mentor);
+    }
+
+    @Override
+    @Transactional
+    public Mentor updateMentor(int id, Mentor mentorDetails) {
+        Mentor mentor = findById(id);
+        mentor.setNombreMentor(mentorDetails.getNombreMentor());
+        mentor.setExperiencia(mentorDetails.getExperiencia());
+        mentor.setEspecialidad(mentorDetails.getEspecialidad());
+        mentor.setNroAsesorias(mentorDetails.getNroAsesorias());
+        return mentorRepository.save(mentor);
+    }
+
+    @Override
+    @Transactional
+    public void deleteMentor(int id) {
+        Mentor mentor = findById(id);
+        mentorRepository.delete(mentor);
+    }
+}
