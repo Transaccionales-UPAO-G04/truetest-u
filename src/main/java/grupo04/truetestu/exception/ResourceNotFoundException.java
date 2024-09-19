449c6ab05de002cc59f18ba7373db013c99706d7
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-09-19 11:15:43 -0500

    Resultado Prueba

--- src/main/java/grupo04/truetestu/exception/ResourceNotFoundException.java
+++ src/main/java/grupo04/truetestu/exception/ResourceNotFoundException.java
@@ -1,11 +1,7 @@
 package grupo04.truetestu.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
--- src/main/java/grupo04/truetestu/model/entity/Estudiante.java
+++ src/main/java/grupo04/truetestu/model/entity/Estudiante.java
@@ -32,3 +32,4 @@ public class Estudiante {
     private Plan plan;
 }
 
+
