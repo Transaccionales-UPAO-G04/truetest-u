0e804e22047535d9a593eb4027ae48d19a4fedd0
fab <fabrix336@gmail.com>
2024-09-28 09:04:17 -0500

    Add recurso en el mapeo

---
+++ src/main/java/grupo04/truetestu/model/entity/Recurso.java
@@ -0,0 +1,36 @@
+package grupo04.truetestu.model.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "resultado_prueba")
+public class Recurso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRecurso;
+
+    @Column(name = "link_recurso", nullable = false, length = 300)
+    private String linkRecurso;
+
+    @Column(name = "es_premium", nullable = false)
+    private boolean esPremium;
+
+    @Column(name = "es_favorito", nullable = false)
+    private boolean esFavorito;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_estudiante", referencedColumnName = "idEstudiante",
+            foreignKey = @ForeignKey(name = "FK_recurso_estudiante"))
+    private Estudiante estudiante;
+
+    @ManyToOne
+    @JoinColumn(name = "id_mentor", referencedColumnName = "idMentor",
+            foreignKey = @ForeignKey(name = "FK_recurso_mentor"))
+    private Mentor mentor;
+}
